d537e0a1394984fab5ce02015c4baf7cd3a92ade
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-03-06 15:43:02 +0300

    Kategori iş kuralı eklendi.

---
+++ ToDo.Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ToDo.Entities.Concrate;
+
+namespace ToDo.Business.ValidationRules.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage("Kategori Adı boş geçilemez");
+            RuleFor(t => t.Name).Length(5, 50).WithMessage("Kategori Adı en az 5 karakter en fazla 50 karakter olabilir.");
+        }
+    }
+}
--- ToDo.Business/ValidationRules/FluentValidation/TodoValidator.cs
+++ ToDo.Business/ValidationRules/FluentValidation/TodoValidator.cs
@@ -8,11 +8,11 @@ namespace ToDo.Business.ValidationRules.FluentValidation
     {
         public TodoValidator()
         {
-            RuleFor(p => p.Content).NotEmpty().WithMessage("TODO içeriği boş geçilemez");
-            RuleFor(p => p.Content).Length(5,1000).WithMessage("TODO içeriği en az 5 karakter en fazla 1000 karakter olabilir."); ;
-            RuleFor(p => p.CategoryId).NotEmpty().WithMessage("Kategorisi olmayan TODO eklenemez");
-            RuleFor(p => p.DueDate).GreaterThanOrEqualTo(DateTime.Today).WithMessage("Bitiş tarihi bugünden küçük olamaz");
-            RuleFor(p => p.ReminMeDate).GreaterThanOrEqualTo(DateTime.Today).WithMessage("Hatırlatma tarihi bugünden küçük olamaz");
+            RuleFor(t => t.Content).NotEmpty().WithMessage("TODO içeriği boş geçilemez");
+            RuleFor(t => t.Content).Length(5,1000).WithMessage("TODO içeriği en az 5 karakter en fazla 1000 karakter olabilir."); 
+            RuleFor(t => t.CategoryId).NotEmpty().WithMessage("Kategorisi olmayan TODO eklenemez");
+            RuleFor(t => t.DueDate).GreaterThanOrEqualTo(DateTime.Today).WithMessage("Bitiş tarihi bugünden küçük olamaz");
+            RuleFor(t => t.ReminMeDate).GreaterThanOrEqualTo(DateTime.Today).WithMessage("Hatırlatma tarihi bugünden küçük olamaz");
         }
     }
 }
