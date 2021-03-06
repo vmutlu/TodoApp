3d98ac4217f16e82a6c6e7034466930c1a1fd2a9
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-03-06 15:23:22 +0300

    iş kuralı eklendi.

---
+++ ToDo.Business/ValidationRules/FluentValidation/TodoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using ToDo.Entities.Concrate;
+
+namespace ToDo.Business.ValidationRules.FluentValidation
+{
+    public class TodoValidator : AbstractValidator<Todo>
+    {
+        public TodoValidator()
+        {
+            RuleFor(p => p.Content).NotEmpty().WithMessage("TODO içeriği boş geçilemez");
+            RuleFor(p => p.Content).Length(5,1000).WithMessage("TODO içeriği en az 5 karakter en fazla 1000 karakter olabilir."); ;
+            RuleFor(p => p.CategoryId).NotEmpty().WithMessage("Kategorisi olmayan TODO eklenemez");
+            RuleFor(p => p.DueDate).GreaterThanOrEqualTo(DateTime.Today).WithMessage("Bitiş tarihi bugünden küçük olamaz");
+            RuleFor(p => p.ReminMeDate).GreaterThanOrEqualTo(DateTime.Today).WithMessage("Hatırlatma tarihi bugünden küçük olamaz");
+        }
+    }
+}
