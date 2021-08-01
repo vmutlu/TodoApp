9e356a6f9f04b78d958073bded3899ce44e17c4e
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-08-01 14:28:43 +0300

    Sayfalama için Core katmanına ortak servis sınıfı eklendi.

---
+++ ToDo.Core/Services/Abstract/IPaginationUriService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ToDo.Core.Utilities.Results;
+
+namespace ToDo.Core.Services.Abstract
+{
+    public interface IPaginationUriService
+    {
+        public Uri GetPageUri(PaginationQuery paginationQuery);
+    }
+}
---
+++ ToDo.Core/Services/Concrete/PaginationUriService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using ToDo.Core.Extensions;
+using ToDo.Core.Services.Abstract;
+using ToDo.Core.Utilities.Results;
+
+namespace ToDo.Core.Services.Concrete
+{
+    public class PaginationUriService : IPaginationUriService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public PaginationUriService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Uri GetPageUri(PaginationQuery paginationQuery)
+        {
+            var baseUri = _httpContextAccessor.GetRequestUri();
+            var route = _httpContextAccessor.GetRoute();
+
+            var endpoint = new Uri(string.Concat(baseUri, route));
+            var queryUri = QueryHelpers.AddQueryString($"{endpoint}", "pageNumber", $"{paginationQuery.PageNumber}");
+
+            queryUri = QueryHelpers.AddQueryString(queryUri, "pageSize", $"{paginationQuery.PageSize}");
+            return new Uri(queryUri);
+        }
+    }
+}
