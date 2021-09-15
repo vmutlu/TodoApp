9c7e528565bfb9371f859e20b7c4948c98782e74
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-09-15 22:16:20 +0300

    Filtreleme nesnesi eklendi

---
+++ ToDo.Core/Entities/GeneralFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.Core.Entities
+{
+    public class GeneralFilter : IPagingFilter
+    {
+        public int Page { get; set; }
+        public string PropertyName { get; set; }
+        public bool Asc { get; set; } = true;
+        public int EntityId { get; set; }
+    }
+}
---
+++ ToDo.Core/Entities/IPagingFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.Core.Entities
+{
+    public interface IPagingFilter
+    {
+        int Page { get; set; }
+        string PropertyName { get; set; }
+        bool Asc { get; set; }
+    }
+}
