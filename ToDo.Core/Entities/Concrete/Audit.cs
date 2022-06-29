2d55d6fd95207948c8c71d28baf4dd6a1f70cce3
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-06-29 20:39:40 +0300

    Audit structure established

---
+++ ToDo.Core/Entities/Concrete/Audit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ToDo.Core.Entities.Concrete
+{
+    public class Audit
+    {
+        public int Id { get; set; }
+        public string TableName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string KeyValues { get; set; }
+        public string OldValues { get; set; }
+        public string NewValues { get; set; }
+    }
+}
